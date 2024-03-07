afa4f09ff4f52e469f0e6b29d8585c8f5525beab
ymparmar07 <ymparmar7@gmail.com>
2024-03-07 18:32:24 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,22 @@
+import HeroSection from "./components/HeroSection";
+import { useProductContext } from "./context/productcontex";
+import Services from "./components/Services";
+import Trusted from "./components/Trusted";
+const About = () => {
+  const { myName } = useProductContext();
+
+  const data = {
+    name: "Hari impex Ecommerce",
+  };
+
+  return (
+    <>
+      {myName}
+      <HeroSection myData={data} />
+      <Services />
+      <Trusted />
+    </>
+  );
+};
+
+export default About;
---
+++ src/components/Trusted.js
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+
+const Trusted = () => {
+  return (
+    <Wrapper className="brand-section">
+      <div className="container">
+        <h3>Trusted By 1000+ Companies</h3>
+        <div className="brand-section-slider">
+          {/* my 1st img  */}
+          <div className="slide">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image2.png"
+              alt="trusted-brands"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image3.png"
+              alt="trusted-brands"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image4.png"
+              alt="trusted-brands"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image6.png"
+              alt="trusted-brands"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image8.png"
+              alt="trusted-brands"
+            />
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  padding: 9rem 0rem;
+  margin:0rem 6rem;
+
+
+
+  background-color: #e8e5ff;
+
+  border-radius: 2rem;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 2px 0px;
+
+  .brand-section {
+    padding: 12rem 0 0 0;
+    background-color: ${({ theme }) => theme.colors.bg};
+  }
+  h3 {
+    text-align: center;
+    text-transform: capitalize;
+    color: ${({ theme }) => theme.colors.text};
+    font-size: 2rem;
+    font-weight: bold;
+  }
+
+  img {
+    min-width: 10rem;
+    height: 10rem;
+  }
+
+  .brand-section-slider {
+    margin-top: 3.2rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+   
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .brand-section-slider {
+      margin-top: 3.2rem;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      /* background-color: red; */
+      text-align: center;
+    }
+  }
+`;
+
+export default Trusted;
